ea7feaa77cceaea0fd73cdaafb21c025549b31b3
Nick Nissen <nickknissen@gmail.com>
2018-08-27 19:56:28 +0200

    Add merchant to to config

--- src/config/quickpay.php
+++ src/config/quickpay.php
@@ -1,8 +1,9 @@
 <?php
 
-
 return [
     'api_key' => env('QUICKPAY_API_KEY'),
     'login' => env('QUICKPAY_LOGIN'),
     'password' => env('QUICKPAY_PASSWORD'),
+    'merchant_id' => env('QUICKPAY_MERCHANT_ID'),
 ];
+
