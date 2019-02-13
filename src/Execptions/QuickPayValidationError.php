b591526d5eecdb46975e6fa4663fe2bf442cbed5
Nick <nickknissen@gmail.com>
2019-02-13 15:00:32 +0100

    Update QuickPayValidationError.php
    Fix quickpay validation execption

--- src/Execptions/QuickPayValidationError.php
+++ src/Execptions/QuickPayValidationError.php
@@ -9,10 +9,9 @@ class QuickPayValidationError extends \Exception
     public function __construct(Response $quickPayResponse)
     {
         $obj = $quickPayResponse->asObject();
-        $errorMessage = sprintf('%s %s', key($obj->errors),reset($obj->errors)[0]);
 
         parent::__construct(
-            $errorMessage,
+            $obj->message,
             $quickPayResponse->httpStatus(),
             null
         );
